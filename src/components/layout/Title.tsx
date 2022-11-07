58399620ec011199ee3c738120e98532be4978da
ingawei <ingawei@gmail.com>
2022-11-06 20:55:40 -0800

    fixed color issue

--- src/components/layout/Title.tsx
+++ src/components/layout/Title.tsx
@@ -9,8 +9,17 @@ export function Title(props: {
 }) {
   const { text, className, color } = props;
   return (
-    <div className={clsx(`text-xl font-bold text-${color}`, className)}>
+    <div className={clsx(`text-xl font-bold`, className, getColor(color))}>
       {text}
     </div>
   );
+}
+
+function getColor(color: titleColors) {
+  if (color === 'pink') {
+    return 'text-pink';
+  }
+  if (color === 'teal') {
+    return 'text-teal';
+  }
 }
